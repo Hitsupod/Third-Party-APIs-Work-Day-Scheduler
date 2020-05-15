a761be519dc6abe3bc95dfbe01d96da7389d3a95
Reggie Littleton <Hitsupod@gmail.com>
2020-05-15 11:50:58 -0500

    Added Time, test for before & after

--- script.js
+++ script.js
@@ -1,38 +1,35 @@
-// var todoInput = document.querySelector("#todo-text");
-// var todoForm = document.querySelector("#todo-form");
-// var todoList = document.querySelector("#todo-list");
-// var todoCountSpan = document.querySelector("#todo-count");
-var Input = document.querySelector('#username');
+var objToday = new Date(),
+	weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
+	dayOfWeek = weekday[objToday.getDay()],
+	domEnder = function() { var a = objToday; if (/1/.test(parseInt((a + "").charAt(0)))) return "th"; a = parseInt((a + "").charAt(1)); return 1 == a ? "st" : 2 == a ? "nd" : 3 == a ? "rd" : "th" }(),
+	dayOfMonth = today + ( objToday.getDate() < 10) ? '0' + objToday.getDate() + domEnder : objToday.getDate() + domEnder,
+	months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
+	curMonth = months[objToday.getMonth()],
+	curYear = objToday.getFullYear(),
+	curHour = objToday.getHours() > 12 ? objToday.getHours() - 12 : (objToday.getHours() < 10 ? "0" + objToday.getHours() : objToday.getHours()),
+	curMinute = objToday.getMinutes() < 10 ? "0" + objToday.getMinutes() : objToday.getMinutes(),
+	curSeconds = objToday.getSeconds() < 10 ? "0" + objToday.getSeconds() : objToday.getSeconds(),
+	curMeridiem = objToday.getHours() > 12 ? "PM" : "AM";
+var today = curHour + ":" + curMinute + "." + curSeconds + curMeridiem + " " + dayOfWeek + " " + dayOfMonth + " of " + curMonth + ", " + curYear;
+
+document.getElementsByTagName('h3')[0].textContent = today;
+
+var Input = document.querySelector('#usernote');
 var btnPress = document.querySelector('#btn');
-var inputSpan = document.querySelector('#filler');
+
 
 
 console.log(btnPress);
-// var todos = [];
-// Create 1 function, 24 inputs and 24 save buttons each button 
-// would save the corresponding input to local storage 
-// if button 1 is click save and display input 1
-// var elUserName = document.getElementById('username');
-// var elMsg = document.getElementById('feedback');
-// function renderNote(){
-//  var input = localStorage.getItem('username');
-//  if(username === null) {
-//    return;
-//  }
-//  inputSpan.textContent = input;
-//}
+
 
 // Function Save Button 
 $('#btn').click(function() {
   event.preventDefault();
   // Event Listener for Click 
   var btnPress = document.querySelector('#btn')
-  var elInput = document.querySelector('#username').value;
-  var elOutput = document.querySelector('#filler').value;
+  var elInput = document.querySelector('#usernote').value;
   //alert('button has been click');
-
   // Take and Store Input 
-  localStorage.setItem('elOutput', elOutput);
   localStorage.setItem('elInput', elInput);
   // Display in Output 
   if(username === null) {
